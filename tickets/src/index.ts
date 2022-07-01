7944ba7ba7cd8a14cf56ccbec5cfeeb07753b0f3
RoninCoder <iqbalkhan2020@protonmail.com>
2022-07-01 17:12:13 +0100

    Create Tickets Service - deployment, skaffold updates

---
+++ tickets/src/index.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+import { errorHandler, NotFoundError } from "@iqtickets/common";
+
+const start = async () => {
+  // Check ENV variables
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+  //Connect to MongoDB
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.log(err);
+  }
+  app.listen(3000, () => {
+    console.log("Listening on port 3000......!");
+  });
+};
+
+start();
