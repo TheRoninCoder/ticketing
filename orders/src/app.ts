e0880451272dc8164ab43524ac4f54d2724bdcb5
RoninCoder <iqbalkhan2020@protonmail.com>
2022-07-13 17:19:28 +0100

    Added order routes

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -4,10 +4,11 @@ import { json } from "body-parser";
 import mongoose from "mongoose";
 import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser } from "@iqtickets/common";
-import { createTicketRouter } from "./routes/new";
-import { showTicketRouter } from "./routes/show";
-import { indexTicketRouter } from "./routes/index";
-import { updateTicketRouter } from "./routes/update";
+
+import { deleteOrderRouter } from "./routes/delete";
+import { newOrderRouter } from "./routes/new";
+import { showOrderRouter } from "./routes/show";
+import { indexOrderRouter } from "./routes/index";
 
 const app = express();
 app.set("trust proxy", true);
@@ -20,10 +21,10 @@ app.use(
 );
 app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(deleteOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
