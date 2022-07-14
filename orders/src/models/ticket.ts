dd929585d3619e60201bdf4f14c632367e437049
RoninCoder <iqbalkhan2020@protonmail.com>
2022-07-14 23:11:36 +0100

    Creates order routes and tests

---
+++ orders/src/models/ticket.ts
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+import { Order, OrderStatus } from "./orders";
+
+interface TicketAttrs {
+  title: string;
+  price: number;
+}
+
+export interface TicketDoc extends mongoose.Document {
+  title: string;
+  price: number;
+  isReserved(): Promise<boolean>;
+}
+
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(attrs: TicketAttrs): TicketDoc;
+}
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket(attrs);
+};
+
+// Run query to look at all orders. FInd an order where the ticket
+// is the ticket we found and order status is not cancelled
+ticketSchema.methods.isReserved = async function () {
+  // this === the ticket doc that we just called 'isReserved' on
+  const existingOrder = await Order.findOne({
+    ticket: this,
+    status: {
+      $in: [
+        OrderStatus.Created,
+        OrderStatus.AwaitingPayment,
+        OrderStatus.Complete,
+      ],
+    },
+  });
+
+  return !!existingOrder;
+};
+
+// Create tciket model
+const Ticket = mongoose.model<TicketDoc, TicketModel>("Ticket", ticketSchema);
+
+export { Ticket };
