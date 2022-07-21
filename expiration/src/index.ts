cfe31717b790c5d925e3bf35046a2678273dc05d
RoninCoder <iqbalkhan2020@protonmail.com>
2022-07-21 11:37:25 +0100

    Expiration Service setup

---
+++ expiration/src/index.ts
@@ -0,0 +1,40 @@
+import { natsWrapper } from "./nats-wrapper";
+
+const start = async () => {
+  // Check ENV variable JWT_KEY exists
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must be defined");
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL must be defined");
+  }
+
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be defined");
+  }
+
+  //Connect to MongoDB and NATS
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+
+    // close down if connection is lost
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+
+    // close down the listener if not available, signal discruption
+    process.on("SIGNIT", () => natsWrapper.client.close());
+    process.on("SIGNTERM", () => natsWrapper.client.close());
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
