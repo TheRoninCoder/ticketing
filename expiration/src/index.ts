ca93d037cda32d2077597bdb66feb2d14a391860
RoninCoder <iqbalkhan2020@protonmail.com>
2022-07-24 13:47:45 +0100

    Initital setup of payment service

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -2,41 +2,32 @@ import { natsWrapper } from "./nats-wrapper";
 import { OrderCreatedListener } from "./events/listeners/order-created-listener";
 
 const start = async () => {
-  // Check ENV variable JWT_KEY exists
-
   if (!process.env.NATS_CLIENT_ID) {
     throw new Error("NATS_CLIENT_ID must be defined");
   }
-
   if (!process.env.NATS_URL) {
     throw new Error("NATS_URL must be defined");
   }
-
   if (!process.env.NATS_CLUSTER_ID) {
     throw new Error("NATS_CLUSTER_ID must be defined");
   }
 
-  //Connect to MongoDB and NATS
   try {
     await natsWrapper.connect(
       process.env.NATS_CLUSTER_ID,
       process.env.NATS_CLIENT_ID,
       process.env.NATS_URL
     );
-
-    // close down if connection is lost
     natsWrapper.client.on("close", () => {
       console.log("NATS connection closed!");
       process.exit();
     });
-
-    // close down the listener if not available, signal discruption
-    process.on("SIGNIT", () => natsWrapper.client.close());
-    process.on("SIGNTERM", () => natsWrapper.client.close());
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
 
     new OrderCreatedListener(natsWrapper.client).listen();
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
 };
 
