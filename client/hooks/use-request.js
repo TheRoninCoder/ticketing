233f892e6a07ee1f95c555e3fe2effabed1fa147
RoninCoder <iqbalkhan2020@protonmail.com>
2022-08-09 18:55:34 +0100

    Created all the ticket and order UI

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -5,10 +5,10 @@ const errorHandling = ({ url, method, body, onSuccess }) => {
   // method must = 'post, 'get', 'patch'
   const [errors, setErrors] = useState(null);
 
-  const doRequest = async () => {
+  const doRequest = async (props = {}) => {
     try {
       setErrors(null);
-      const response = await axios[method](url, body);
+      const response = await axios[method](url, { ...body, ...props });
 
       if (onSuccess) {
         onSuccess(response.data);
