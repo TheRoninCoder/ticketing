ff98cde919dee6b3da59270edd3e0dea355ee386
RoninCoder <iqbalkhan2020@protonmail.com>
2022-08-12 11:17:57 +0100

    test

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -6,6 +6,7 @@ import { OrderCreatedListener } from "./events/listeners/order-created-listener"
 
 const start = async () => {
   console.log("Starting......");
+  console.log("Starting......");
   // Check ENV variable JWT_KEY exists
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
